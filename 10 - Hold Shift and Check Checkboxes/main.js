06f36f64a1f4ff115fd7b964fe7c03ab7eaa0df1
JinWen <onlystp417@gmail.com>
2020-05-18 13:30:26 +0800

    [feature]: JS30_10 Hold Shift and Check Checkboxes basic complete

--- 10 - Hold Shift and Check Checkboxes/main.js
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -1,12 +1,30 @@
-const items = document.querySelectorAll(".item");
+// 選到所有的 checkboxs
+const checkboxs = document.querySelectorAll("input");
 
-items.forEach((item, index) => item.addEventListener("click", checkFirstItem));
-// items.forEach((item, index) => item.addEventListener("click", itemsSequently));
+// 聲明第一個勾選的變數
+let firstChecked = null;
 
-function checkFirstItem(index) {
-	const input = this.querySelector("input");
-	input.checked = true;
-	console.log(this);
+function checkItems(e) {
+	// 聲明一個是否在這兩個 checkbox 中間（包含兩個 checkbox）的區域變數為 false
+	let inBetween = false;
+	// 如果 shift + click，就開始遍歷每個 checkbox
+	if (e.shiftKey && this.checked) {
+		checkboxs.forEach(checkbox => {
+			// 符合第二個 shift + click 的 checkbox 或是 符合第一個 checkbox
+			if (checkbox === this || checkbox === firstChecked) {
+				// 如果符合其中一個就會反轉為 true
+				// 所以如果遍歷到第二次反轉，就會再轉回 false
+				inBetween = !inBetween;
+			}
+			// 若 inBetween 為真，checkbox 就勾選
+			if (inBetween) {
+				checkbox.checked = true;
+			}
+		});
+	}
+	// 將第一個勾選的 checkbox 放函式最後，以免被第二個 shift + click 蓋掉
+	firstChecked = this;
 }
 
-// function itemsSequently() {}
+// 為每個 checkbox 掛上滑鼠點擊監聽
+checkboxs.forEach(checkbox => checkbox.addEventListener("click", checkItems));
