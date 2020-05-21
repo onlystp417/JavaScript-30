8eef0add759a49d58727fbdbc8316e935b576efe
JinWen <onlystp417@gmail.com>
2020-05-21 12:08:05 +0800

    [performance]: JS30_13 Slide in on Scroll coding sequence

--- 13 - Slide in on Scroll/main.js
+++ 13 - Slide in on Scroll/main.js
@@ -1,10 +1,7 @@
-// 拜訪到圖片
-const sliderImages = document.querySelectorAll(".slide-in");
-
 // 在網頁上掛滾輪監聽，每 20 毫秒再執行 slideIn 函式
 window.addEventListener("scroll", debounce(slideIn));
 
-// 每 20 毫秒再執行函式
+// 緩 20 毫秒再執行函式
 function debounce(func, wait = 20, immediate = true) {
 	var timeout;
 	return function () {
@@ -21,15 +18,18 @@ function debounce(func, wait = 20, immediate = true) {
 	};
 }
 
+// 拜訪到圖片
+const sliderImages = document.querySelectorAll(".slide-in");
+
 // 圖片滑進來的效果
 function slideIn(e) {
 	sliderImages.forEach(sliderImage => {
 		// 取得圖片一半高度時的位置
 		const slideInAt = window.scrollY + window.innerHeight - sliderImage.height / 2;
-		// 取得圖片底部的位置
-		const imageBottom = sliderImage.offsetTop + sliderImage.height;
 		// scroll 到圖片一半時
 		const isHalfShown = slideInAt > sliderImage.offsetTop;
+		// 取得圖片底部的位置
+		const imageBottom = sliderImage.offsetTop + sliderImage.height;
 		// 未 scroll 過圖片的區域
 		const isNotScrollPast = window.scrollY < imageBottom;
 		// scroll 到圖片一半，且未 scroll 過圖片的區域，就執行圖片滑進來的效果
