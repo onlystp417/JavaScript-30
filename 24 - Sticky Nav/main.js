f9d83b6a02f7b373cf8a6817d80daa73641e7c78
JinWen <onlystp417@gmail.com>
2020-06-29 11:59:17 +0800

    [feat]: JS_24_Sticky_Nav complete

---
+++ 24 - Sticky Nav/main.js
@@ -0,0 +1,24 @@
+// 取得 navbar 節點
+const navbar = document.querySelector('#main');
+
+// 取得 navbar 上緣距離視口的高度
+const navbarTopSpace = navbar.offsetTop;
+// 取得 navbar 的元素盒高度
+const navbarHeight = navbar.offsetHeight;
+
+// 在視窗上綁定 scroll 事件，只要一觸發就執行 navFixed
+window.addEventListener('scroll', navFixed);
+
+// 讓 navbar 在特定條件下黏再視口上緣的功能
+function navFixed() {
+  // 如果 scroll 的高度大於等於 navbarTopSpace，就讓 navbar 固定在視口上緣
+  // 因為 navbar 的 position 脫離 normal flow，所以要把它高度家回來，才不會造成畫面跳動
+  if(window.scrollY >= navbarTopSpace) {
+    document.body.classList.add('navbar-effects');
+    document.body.style.paddingTop = navbarHeight + 'px';
+  } else {
+    // 如果不符合條件，就回復初始狀態
+    document.body.classList.remove('navbar-effects');
+    document.body.style.paddingTop = 0 + 'px';
+  }
+}
